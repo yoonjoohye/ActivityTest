2ba4673b5b797f2937a864f1d8d365b9cf462d2c
yoonjoohye <67yjh00@gmail.com>
2017-04-13 20:15:03 +0900

    #3 event programming

--- app/src/main/java/a67yjh00/com/example/lg/activitytest/MainActivity.java
+++ app/src/main/java/a67yjh00/com/example/lg/activitytest/MainActivity.java
@@ -1,7 +1,11 @@
 package a67yjh00.com.example.lg.activitytest;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button butMain=(Button)findViewById(R.id.but_main);
+        butMain.setOnClickListener(butMainHandler);
     }
+    View.OnClickListener butMainHandler=new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            Intent intent=new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
+    };
 }
--- app/src/main/java/a67yjh00/com/example/lg/activitytest/SecondActivity.java
+++ app/src/main/java/a67yjh00/com/example/lg/activitytest/SecondActivity.java
@@ -2,6 +2,8 @@ package a67yjh00.com.example.lg.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button butback = (Button) findViewById(R.id.but_second);
+        butback.setOnClickListener(butbackHandler);
     }
+    View.OnClickListener butbackHandler=new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            finish();//사라지게함
+        }
+    };
 }
