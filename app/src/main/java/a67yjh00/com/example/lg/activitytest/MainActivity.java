b8e6661af560d395cb2f1ca9078b988240453eb9
yoonjoohye <67yjh00@gmail.com>
2017-04-13 20:50:53 +0900

    #5 pass secondactivity

--- app/src/main/java/a67yjh00/com/example/lg/activitytest/MainActivity.java
+++ app/src/main/java/a67yjh00/com/example/lg/activitytest/MainActivity.java
@@ -6,9 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editName, editTel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,11 +17,17 @@ public class MainActivity extends AppCompatActivity {
 
         Button butMain=(Button)findViewById(R.id.but_main);
         butMain.setOnClickListener(butMainHandler);
+        editName=(EditText)findViewById(R.id.edit_name);
+        editTel=(EditText)findViewById(R.id.edit_tel);
     }
     View.OnClickListener butMainHandler=new View.OnClickListener(){
         @Override
         public void onClick(View v) {
             Intent intent=new Intent(MainActivity.this, SecondActivity.class);
+            String name=editName.getText().toString();
+            String tel=editTel.getText().toString();
+            intent.putExtra("name",name);
+            intent.putExtra("tel",tel);
             startActivity(intent);
         }
     };
--- app/src/main/java/a67yjh00/com/example/lg/activitytest/SecondActivity.java
+++ app/src/main/java/a67yjh00/com/example/lg/activitytest/SecondActivity.java
@@ -1,12 +1,14 @@
 package a67yjh00.com.example.lg.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    String name, tel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +16,11 @@ public class SecondActivity extends AppCompatActivity {
 
         Button butback = (Button) findViewById(R.id.but_second);
         butback.setOnClickListener(butbackHandler);
+        Intent intent=getIntent();
+        name=intent.getStringExtra("name");
+        tel=intent.getStringExtra("tel");
+        TextView textResult=(TextView)findViewById(R.id.text_result);
+        textResult.setText("*설명: "+name+"\n"+"*전화번호: "+tel);
     }
     View.OnClickListener butbackHandler=new View.OnClickListener(){
         @Override
